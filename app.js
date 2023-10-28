8b48ddfb58ff41dd34680139e6cfae9922767dbf
bkay-stack <kevinbest1900@gmail.com>
2023-10-28 18:07:21 +0100

    new code

--- app.js
+++ app.js
@@ -2,27 +2,30 @@ const form = document.querySelector('form');
 const input = document.querySelector('input');
 const btn = document.querySelector('button');
 const errorMsg = document.querySelector('.error');
+const borderError = document.querySelector('.border-error')
 
 
 
 
     //   Email Validate  
 
-const emailValue = input.value.trim();
+// const emailValue = input.value.trim();
 
-const emailPattern = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/;
+// const emailPattern = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/;
 
   btn.addEventListener('click', (e) => {
-      if (emailValue !== emailPattern) {
+      if (input.value === '') {
          e.preventDefault();
          errorMsg.style.display = 'block';
+         borderError.style.border = '1px solid hsl(354, 100%, 66%)'
          setTimeout(() => {
             errorMsg.style.display = 'none';
-            console.log(errorMsg);
-        const clear = input.value = '';
+            borderError.style.border = '';
+            const clear = input.value = '';
          
          },1000)
-      } else {
-          errorMsg.style.display = 'none'
-      }
+      }  
   })
+
+
+  console.log(btn)
