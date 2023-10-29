b39a9d2da9e9dd69f6a6163abe8682abafbf2584
bkay-stack <kevinbest1900@gmail.com>
2023-10-29 09:23:29 +0100

    New

--- app.js
+++ app.js
@@ -9,20 +9,22 @@ const borderError = document.querySelector('.border-error')
 
     //   Email Validate  
 
-// const emailValue = input.value.trim();
-
-// const emailPattern = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/;
-
-  btn.addEventListener('click', (e) => {
-      if (input.value === '') {
-         e.preventDefault();
-         errorMsg.style.display = 'block';                      
-         borderError.style.border = '1px solid hsl(354, 100%, 66%)';
-         setTimeout(() => {
-            errorMsg.style.display = 'none';  
-            borderError.style.border = '';
-            const clear = input.value = '';
-         
-         },1000)
-      }  
-  })
+    const emailPattern = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/;
+    
+    btn.addEventListener('click', (e) => {
+        const emailValue = input.value.trim();
+        
+        if (!emailPattern.test(emailValue)) { 
+            e.preventDefault();
+            errorMsg.style.display = 'block';
+            borderError.style.border = '1px solid hsl(354, 100%, 66%)';
+            
+            setTimeout(() => {
+                errorMsg.style.display = 'none';
+                borderError.style.border = '';
+                input.value = '';
+                
+            }, 1000);
+        }
+    });
+    
